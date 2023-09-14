403d3afe2173193d06d1eda776ffeb1bde1d1cb6
Rafos2423 <vladp2003kek@gmail.com>
2023-09-14 15:27:47 +0300

    -

--- 1/1.py
+++ 1/1.py
@@ -6,8 +6,8 @@ higher_arr=np.array([[0.0, -1.0, 0.0],[-1.0, 4.0, -1.0],[0.0, -1.0, 0.0]])
 sharp_arr=np.array([[-1.0, -1.0, -1.0],[-1.0, 9.0, -1.0],[-1.0, -1.0, -1.0]])
 
 for i in range(1,6):
-  img_src = cv2.imread(f"images//{i}.jpg")
-
+  img_src = cv2.imread("1.jpg")
+  
   kernel_lower = lower_arr/(np.sum(lower_arr) if np.sum(lower_arr)!=0 else 1)
   kernel_higher = higher_arr/(np.sum(higher_arr) if np.sum(higher_arr)!=0 else 1)
   kernel_sharp = sharp_arr/(np.sum(sharp_arr) if np.sum(higher_arr)!=0 else 1)
@@ -18,4 +18,4 @@ for i in range(1,6):
 
   result_image = np.concatenate((img_lower_rst, img_higher_rst, img_sharp), axis=1)
 
-  cv2.imshow(result_image)
+  cv2.imshow("Result", result_image)
